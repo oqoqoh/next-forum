97b775264abc3c01197a575879fd2ea82e82b8ef
Kyuyoung Oh <oqoq59@gmail.com>
2023-05-22 11:40:56 +0900

    본인 작성글만 수정삭제 아이콘 표시

--- app/list/ListItem.js
+++ app/list/ListItem.js
@@ -3,46 +3,48 @@ import MoveToUpdateButton from "../list/MoveToUpdateButton";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
-
-
 export default function ListItem({ result, session }) {
   const router = useRouter();
-  console.log(session)
-  
+  let sessionInfo;
+
+  if (session) {
+    sessionInfo = JSON.parse(session.value);
+  }
+
   return (
     <>
       {result.map((data, i) => {
         return (
-
           <div className="list-item" key={i}>
             <Link href={`/list/${data._id}`}>
               <h4>{data.title}</h4>
             </Link>
-            {
-              session.value != 'null' ? <MoveToUpdateButton id={data._id.toString()} />
-              : <></>
-            }
-            
-            
-            <p
-              onClick={() => {
-                
-                
-                fetch("/api/post/delete", {
-                  method: "POST",
-                  body: data._id.toString(),
-                
-                })
-                  .then((res) => res.json())
-                  .then((result) => {
-                    console.log(result);
-                    router.push("/list");
-                  });
-              }}
-            >
-              ❌
-            </p>
-            
+            {sessionInfo ? (
+              sessionInfo.user.email == data.auth ? (
+                <div>
+                  <MoveToUpdateButton id={data._id.toString()} />
+                  <p
+                    onClick={() => {
+                      fetch("/api/post/delete", {
+                        method: "POST",
+                        body: data._id.toString(),
+                      })
+                        .then((res) => res.json())
+                        .then((result) => {
+                          console.log(result);
+                          router.push("/list");
+                        });
+                    }}
+                  >
+                    ❌
+                  </p>
+                </div>
+              ) : (
+                <></>
+              )
+            ) : (
+              <></>
+            )}
           </div>
         );
       })}
